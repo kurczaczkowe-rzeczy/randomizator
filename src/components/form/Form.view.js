fa0e42ceeaaa147eda54a76477b638114a02a818
Magdalena Rowicka <magdalena.rowicka@mayeryn.com>
2020-10-03 23:45:54 +0200

    create form components

---
+++ src/components/form/Form.view.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Label from 'components/form/components/label/Label.view';
+import TextInput from 'components/form/components/textInput/TextInput.view';
+import classes from './form.module.scss';
+import Button from 'components/form/components/button/Button.view';
+
+const Form = () => (
+  <form className={classes.form}>
+    <div className={classes[ 'align-bottom' ]}>
+      <Label content="Imie męskie" required />
+      <TextInput required />
+    </div>
+
+    <div className={classes[ 'align-bottom' ]}>
+      <Label content="Imie damskie" required />
+      <TextInput required />
+    </div>
+
+    <Button value="Wyślij" type="submit" />
+  </form>
+);
+
+Form.propTypes = {};
+
+export default Form;
