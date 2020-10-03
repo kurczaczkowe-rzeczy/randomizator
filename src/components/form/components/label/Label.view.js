61ca544f83198749c2b3789365d314c6c1a3444a
Magdalena Rowicka <magdalena.rowicka@mayeryn.com>
2020-10-03 22:19:32 +0200

    create label component

---
+++ src/components/form/components/label/Label.view.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classes from './label.module.scss';
+
+const Label = ({ content, required }) => (
+  <span className={ classes }>
+    { content }
+    {required && '*'}
+  </span>
+);
+
+Label.propTypes = {
+  content: PropTypes.string.isRequired,
+  required: PropTypes.bool,
+};
+
+Label.defaultProps = { required: false };
+export default Label;
