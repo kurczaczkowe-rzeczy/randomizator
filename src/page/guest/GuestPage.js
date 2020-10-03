858cec2892887861ee1ad24e21b642aceee6bcd7
Magdalena Rowicka <magdalena.rowicka@mayeryn.com>
2020-10-03 23:45:35 +0200

    create textInput components

--- src/page/guest/GuestPage.js
+++ src/page/guest/GuestPage.js
@@ -1,11 +1,10 @@
-import Description from 'components/description/Description.view';
+import Form from 'components/form';
 import React from 'react';
 
 const GuestPage = () => (
   <div>
     <h1>Guest panel</h1>
-    <Description label="Twórca" content="Szczęśliwy Ziemniak" />
-    <Description label="Formularz" content="Imiona zwierząt anglojęzycznych" />
+    <Form />
   </div>
 );
 
