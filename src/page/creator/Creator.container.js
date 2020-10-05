c9d60f0e758d1e1aee166d4bd479f4600c2612d4
Magdalena Rowicka <magdalena.rowicka@mayeryn.com>
2020-10-05 09:22:55 +0200

    sort array by keys

--- src/page/creator/Creator.container.js
+++ src/page/creator/Creator.container.js
@@ -3,13 +3,16 @@ import firebase from 'config/firebaseConfig';
 import CreatorView from './Creator.view';
 import { useLocation } from 'react-router';
 import useStateWithCallback from 'use-state-with-callback';
+import _isNil from 'lodash/isNil';
 
 const useAnswers = () => {
-
-  const [ load, setLoad ] = useState( false );
-  const [ answers, setAnswers ] = useStateWithCallback([], ( answers ) => {
-    setLoad( answers.length > 0 );
+  const [ data, setData ] = useStateWithCallback([], ( data ) => {
+    if ( data !== []) {
+      setDataLoad( true );
+    }
   });
+  const [ dataLoad, setDataLoad ] = useState( false );
+  const [ answers, setAnswers ] = useState([]);
   const history = useLocation();
 
   useEffect(() => {
@@ -18,25 +21,53 @@ const useAnswers = () => {
     const unsub = firebase.collection( pathArray[ 2 ])
       .onSnapshot((( snapshot ) => {
         snapshot.docs.forEach(( doc ) => {
-          setAnswers( doc.data().answers );
+          const ans = doc.data().answers;
+
+          setAnswers( ans );
+          const result = getData( ans );
+
+          setData( result );
         });
       }));
 
     return () => unsub();
   }, [ ]); // eslint-disable-line react-hooks/exhaustive-deps
 
-  return [ answers, load ];
+  return [
+    answers,
+    data,
+    dataLoad,
+  ];
+};
+
+const getData = ( answers ) => {
+  const result = { };
+
+  for ( let i = 0;i < answers.length;i++ ) {
+    for ( const [ key, value ] of Object.entries( answers[ i ])) {
+      if ( _isNil( result[ key ])) {
+        result[ key ] = [];
+      }
+      result[ key ].push( value );
+    }
+  }
+
+  return result;
 };
 
 const Creator = () => {
-  const [ anws, load ] = useAnswers();
+  const [
+    answers,
+    data,
+    dataLoad,
+  ] = useAnswers();
 
   const drawResult = () => {
-    console.log( anws );
-    console.log( load );
+    console.log( answers );
+    console.log( data );
   };
 
-  return ( <CreatorView onRandomClick={drawResult} loadedData={load} /> );
+  return ( <CreatorView onRandomClick={drawResult} loadedData={dataLoad} /> );
 };
 
 export default Creator;
