ce8638077bef49971d5b7b6a28201be78c3529bd
Magdalena Rowicka <magdalena.rowicka@mayeryn.com>
2020-10-11 12:05:32 +0200

    Updates

---
+++ precache-manifest.b79200199f464b1d3c3fc05ad8460b5e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bc0c020badcf5dc3ca71f7a9a880af0",
+    "url": "/randomizator/index.html"
+  },
+  {
+    "revision": "ce82380e71226f160340",
+    "url": "/randomizator/static/css/main.2f0def65.chunk.css"
+  },
+  {
+    "revision": "45a4710c1bc75419d917",
+    "url": "/randomizator/static/js/2.0346dca3.chunk.js"
+  },
+  {
+    "revision": "8cb2f58f41e146a30da3e80da1f28f0e",
+    "url": "/randomizator/static/js/2.0346dca3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce82380e71226f160340",
+    "url": "/randomizator/static/js/main.625afd2b.chunk.js"
+  },
+  {
+    "revision": "da189dc0e0480b24502c",
+    "url": "/randomizator/static/js/runtime-main.c8d2544f.js"
+  },
+  {
+    "revision": "fb72f8830de8a57a9b26d7dbb6699cf5",
+    "url": "/randomizator/static/media/unicorn.fb72f883.svg"
+  }
+]);
