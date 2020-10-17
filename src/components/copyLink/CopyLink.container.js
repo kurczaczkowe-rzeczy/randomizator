74ea7126dd1b41b4fb4208829561509c6eea70f8
Magdalena Rowicka <magdalena.rowicka@mayeryn.com>
2020-10-17 09:52:09 +0200

    move delay of disappear to constants file

--- src/components/copyLink/CopyLink.container.js
+++ src/components/copyLink/CopyLink.container.js
@@ -5,12 +5,13 @@ import _isEmpty from 'lodash/isEmpty';
 import useStateWithCallback from 'use-state-with-callback';
 
 import CopyLinkView from 'components/copyLink/CopyLink.view';
+import { DELAY_DISAPPEARING } from 'constans';
 
 const CopyLink = ({ formID, userID }) => {
   const [ link, setLink ] = useState( '' );
   const [ copied, setCopied ] = useStateWithCallback( false, ( copied ) => {
     if ( copied ) {
-      setTimeout(() => { setCopied( false ); }, 800 );
+      setTimeout(() => { setCopied( false ); }, DELAY_DISAPPEARING );
     }
   });
 
