cec21b9766c1c734a08693230e3c55a5a2658c7c
Lzygmunt <lzygmunt0@gmail.com>
2020-10-17 21:05:14 +0200

    style select input

--- src/components/select/Select.styles.js
+++ src/components/select/Select.styles.js
@@ -7,12 +7,22 @@ export default makeStyles(() => ({
     '&.Mui-focused': { color: '#bdaeae' },
   },
 
-  labelFocus: {
+  list: { padding: 0 },
+
+  select: {
     '&:focus':
       {
         background: 'transparent',
         color: '#bdaeae',
       },
+    backgroundColor: '#222222',
+    borderLeft: '2px solid',
+    borderRight: '2px solid',
+    borderImage: 'linear-gradient(180deg, #222 0%, #222 49%, #771e76 50%, #771e76 100%) 1',
+    borderBottom: '2px solid',
+    padding: '8px',
+    color: '#bdaeae',
+    fontSize: '17px',
   },
 
   paper: {
@@ -23,11 +33,16 @@ export default makeStyles(() => ({
   menuItem: {
     borderBottom: '1px solid #771e76',
     padding: '8px 16px',
+    '&:last-of-type': { border: 'none' },
   },
 
   menuItemSelected: { background: '#771e76' },
 
-  icon: { fill: '#771e76' },
+  icon: {
+    fill: '#771e76',
+    paddingRight: 8,
+    paddingLeft: 8,
+  },
 
   selected: {
     color: '#fff',
