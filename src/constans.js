f3756bc7debd16af3beb8759d8b4d2ef40ee484b
Lzygmunt <lzygmunt0@gmail.com>
2020-11-07 18:55:43 +0100

    get homepage from package.json

--- src/constans.js
+++ src/constans.js
@@ -1,4 +1,3 @@
 export const DELAY_DISAPPEARING = 800;
 export const FORM_ID_KEY = 'FORM_ID';
-export const HOME_PAGE = process.env.REACT_APP_HOMEPAGE;
-export { version as APP_VERSION } from '../package.json';
+export { version as APP_VERSION, homepage as HOME_PAGE } from '../package.json';
