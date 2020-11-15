3128c3d16aab1ab8495e1268f951f6900f3ad170
Magdalena Rowicka <magdalena.rowicka@mayeryn.com>
2020-11-15 15:02:05 +0100

    fix from PR

--- src/page/creator/CreatorPage.container.js
+++ src/page/creator/CreatorPage.container.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { useLocation } from 'react-router';
 import _isNil from 'lodash/isNil';
 import _forEach from 'lodash/forEach';
-import _find from 'lodash/find';
+import _union from 'lodash/union';
 import { connect } from 'react-redux';
 
 import { clearDraw, setDrawResult } from 'store/actions/drawAction';
@@ -34,7 +34,7 @@ const Creator = ({
     const found = forms.findIndex(( form ) => form.id === formID );
 
     if ( found === -1 ) {
-      localStorage.setItem( FORM_ID_KEY, forms[ 0 ].id  );
+      localStorage.setItem( FORM_ID_KEY, forms[ 0 ].id );
       setFormID( forms[ 0 ].id );
     }
   };
@@ -84,9 +84,7 @@ const Creator = ({
           result[ key ] = [];
         }
 
-        if ( !_find( result[ key ], ( val ) => val === value )) {
-          result[ key ].push( value );
-        }
+        result[ key ] = _union( result[ key ], [ value ]);
       });
     });
 
