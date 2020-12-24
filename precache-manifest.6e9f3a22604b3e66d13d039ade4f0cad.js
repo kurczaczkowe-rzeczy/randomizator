ab9663cf801c14e19f3f86a0c468c21a7396fd95
Lzygmunt <lzygmunt0@gmail.com>
2020-12-24 17:44:36 +0100

    Updates

---
+++ precache-manifest.6e9f3a22604b3e66d13d039ade4f0cad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c59f548c5918b25f1d171b7f273b5e41",
+    "url": "/randomizator/index.html"
+  },
+  {
+    "revision": "73cd2a81673e31160012",
+    "url": "/randomizator/static/css/main.fcb2f6fc.chunk.css"
+  },
+  {
+    "revision": "ca0aee80f579f6a80316",
+    "url": "/randomizator/static/js/2.9b7fa1bf.chunk.js"
+  },
+  {
+    "revision": "8cb2f58f41e146a30da3e80da1f28f0e",
+    "url": "/randomizator/static/js/2.9b7fa1bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73cd2a81673e31160012",
+    "url": "/randomizator/static/js/main.afafa527.chunk.js"
+  },
+  {
+    "revision": "da189dc0e0480b24502c",
+    "url": "/randomizator/static/js/runtime-main.c8d2544f.js"
+  },
+  {
+    "revision": "fb72f8830de8a57a9b26d7dbb6699cf5",
+    "url": "/randomizator/static/media/unicorn.fb72f883.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/randomizator/precache-manifest.3d4d1fc453f9fb40b1f95e8c49ac3fa3.js"
+  "/randomizator/precache-manifest.6e9f3a22604b3e66d13d039ade4f0cad.js"
 );
 
 self.addEventListener('message', (event) => {
