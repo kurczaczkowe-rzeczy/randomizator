461cac321f8b741068c65845be1e30e8fe89ea56
RowickaM <rowickam.23@gmail.com>
2021-04-11 11:03:19 +0200

    refactor(Form): change import order, add disable eslint to any

--- src/components/form/Form.container.tsx
+++ src/components/form/Form.container.tsx
@@ -4,18 +4,20 @@ import _isEmpty from 'lodash/isEmpty';
 import _isEqual from 'lodash/isEqual';
 import _some from 'lodash/some';
 
-import FormView from './Form.view';
-import { FormContainer } from 'components/form/Form.types';
 import { RootState } from 'store/reducers/rootReducer';
+
+import FormView from './Form.view';
+import { FormContainer } from './Form.types';
 
 const Form = ({
   preview = false,
-  onSubmit = ():void => {},
-  additionalFunction = ():void => {},
+  onSubmit = (): void => {},
+  additionalFunction = (): void => {},
 }: FormContainer ): JSX.Element => {
 
   const nameOfForm = useSelector(( state: RootState ) => state.form.name );
 
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const someFieldFill = ( ...args: any[]): boolean => _some( args, ( field ) => !_isEmpty( field ));
 
   /* ToDo use constants instead of hardcoded strings */
