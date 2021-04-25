7907af1e3ab406983a881b647e0916b26d14feae
rowickam <rowickam.23@gmail.com>
2021-04-25 15:12:55 +0200

    feat(LoginPage): add strings resource for pl and eng

--- src/page/login/LoginPage.view.tsx
+++ src/page/login/LoginPage.view.tsx
@@ -4,37 +4,42 @@ import TextInput from 'components/form/components/textInput';
 
 import classes from './loginPage.module.scss';
 import { ILogin } from './LoginPage.types';
+import useLocaleString from '../../hooks/useLocaleString';
 
 /**
  * Page displaying login form
  */
-export const Login = ({ onLogin, authError = null }: ILogin ): JSX.Element => (
-  <div className={ classes.center }>
-    <Card
-      cardClass={ classes.card }
-      title="Zaloguj się"
-      body={ (
-        <>
-          <form onSubmit={ onLogin } method="post">
-            <TextInput
-              required
-              name="email"
-              placeholder="email@example.com"
-              type="email"
-            />
-            <TextInput
-              required
-              name="password"
-              placeholder="password"
-              type="password"
-            />
-            <Button value="Zaloguj się" type="submit" />
-          </form>
-          { authError !== null && <div className={ classes.error }>{authError}</div> }
-        </>
-      ) }
-    />
-  </div>
-);
+export const Login = ({ onLogin, authError = null }: ILogin ): JSX.Element => {
+  const getString = useLocaleString();
+
+  return (
+    <div className={ classes.center }>
+      <Card
+        cardClass={ classes.card }
+        title={ getString( 'login' ) }
+        body={ (
+          <>
+            <form onSubmit={ onLogin } method="post">
+              <TextInput
+                required
+                name="email"
+                placeholder={ getString( 'email' ) }
+                type="email"
+              />
+              <TextInput
+                required
+                name="password"
+                placeholder={ getString( 'password' ) }
+                type="password"
+              />
+              <Button value={ getString( 'login' ) } type="submit" />
+            </form>
+            {authError !== null && <div className={ classes.error }>{authError}</div>}
+          </>
+        ) }
+      />
+    </div>
+  );
+};
 
 export default Login;
