160b9baa2be0919434c505a09f353fe4c22d09a2
Lzygmunt <lzygmunt0@gmail.com>
2021-08-28 18:38:08 +0200

    feat(Storybook): add redux decorator

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,27 +1,36 @@
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
+
 import CssBaseline from '@material-ui/core/CssBaseline';
 import ThemeProvider from '@material-ui/styles/ThemeProvider';
 
-import randoTheme from './randoTheme';
+import rootReducer from 'store/reducers/rootReducer';
 import { theme } from 'theme';
 
-const decorators = [
-  (story) => (
+import randoTheme from './randoTheme';
+
+const store = createStore( rootReducer, composeWithDevTools( applyMiddleware( thunk )));
+
+export const decorators = [
+  (Story) => (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      { story() }
+      <Story />
     </ThemeProvider>
   ),
+  (Story) => (
+    <Provider store={store}>
+      <Story />
+    </Provider>
+  )
 ];
 
-const parameters = {
+export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   docs: {
     theme: randoTheme,
     source: { type: 'dynamic' },
   },
-};
-
-export {
-  parameters,
-  decorators,
 };
