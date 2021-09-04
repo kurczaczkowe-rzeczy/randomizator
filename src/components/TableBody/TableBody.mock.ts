0d82b19d2dcc0f9b3055cc83e0c1e653c3e4443a
Lzygmunt <lzygmunt0@gmail.com>
2021-09-04 15:11:49 +0200

    refactor(TableBody): add in mock index column

--- src/components/TableBody/TableBody.mock.ts
+++
@@ -1,7 +0,0 @@
-import _times from 'lodash/times';
-
-import { bodyCells } from 'components/TableRow/TableRow.mock';
-
-const mockData = { rows: _times( 600, ( id ) => ({ id, cells: bodyCells })) };
-
-export default mockData;
---
+++ src/components/TableBody/TableBody.mock.tsx
@@ -0,0 +1,9 @@
+import _times from 'lodash/times';
+
+import { bodyCells } from 'components/TableRow/TableRow.mock';
+
+const cellsGetter = ( id: number ) => [{ id: 'index', children: <span>{ id }</span> }, ...bodyCells ] as const;
+
+const mockData = { rows: _times( 600, ( id ) => ({ id, cells: cellsGetter( id ) })) };
+
+export default mockData;
